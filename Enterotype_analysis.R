2c63ec0da087705b0c203aac834e1dea7c360e43
LixiaPu <167181287+LixiaPu@users.noreply.github.com>
2024-04-17 09:27:48 +0800

    Update Enterotype_analysis.R

--- Enterotype_analysis.R
+++ Enterotype_analysis.R
@@ -13,48 +13,44 @@ library(scales)
 
 genus.its<- read.csv("Long-lived-gut_mycobiome/level-genus.df.csv",sep=",",header=T,strip.white = T, stringsAsFactors = F,check.names=F)
 metadata <- read.csv("Long-lived-gut_mycobiome_project/metadata.csv", header=TRUE, sep = ",",row.names = "SampleID",stringsAsFactors = F,fileEncoding = "GBK")
-df<-merge(metadata,genus.its,by = "Sample_ID")
+df<-merge(metadata,genus.its,by = "SampleID")
 row.names(df)<-df$SampleID
-id<-metadata.noerr$sampleID
-data.16s.df <- subset(df, SampleID %in% id)
-data.16s.df<-data.16s.df%>%dplyr::select(!(1:92))
-df.16s.genus<-t(data.16s.df)
-data.16s.genus<-apply(df.16s.genus,2,as.numeric)
-row.names(data.16s.genus)<-row.names(df.16s.genus)
-data.16s.genus<-apply(data.16s.genus,2,function(x) x/sum(x)*100)
-data.16s.denoized<-data.16s.genus[apply(data.16s.genus,1,function(x) sum(x>0)>=0.2*length(x)),]
-#data.16s.denoized=noise.removal(data.16s.genus,percent=0.0001)
-data.16s.denoized<-data.16s.denoized[!grepl("g__metagenome.1",row.names(data.16s.denoized)),]
+data.df<-data.df%>%dplyr::select(!(1:92))
+data.df<-t(data.df)
+data.genus<-apply(data.df,2,as.numeric)
+row.names(data.genus)<-row.names(data.genus)
+data.genus<-apply(data.genus,2,function(x) x/sum(x)*100)
+data.denoized<-data.genus[apply(data.genus,1,function(x) sum(x>0)>=0.2*length(x)),]
 #PAM cluster +bary
-data.dist.bc <- vegdist(t(data.16s.denoized),method = "bray")
-write.table(as.matrix(data.dist.bc),"bray_curtis-genus_16s.txt",sep = '\t',quote = FALSE,col.names = NA)
+data.dist.bc <- vegdist(t(data.denoized),method = "bray")
+write.table(as.matrix(data.dist.bc),"bray_curtis-genus.txt",sep = '\t',quote = FALSE,col.names = NA)
 #Calinski-Harabasz 
-nclusters.bc = index.G1(t(data.16s.denoized), data.cluster, d = data.dist.bc, centrotypes = "medoids")
+nclusters.bc = index.G1(t(data.denoized), data.cluster, d = data.dist.bc, enterotypes = "medoids")
 nclusters.bc=NULL
 for (k in 1:10) { 
   if (k==1) {
     nclusters.bc[k]=NA 
   } else {
     data.cluster_temp=pam.clustering(data.dist.bc, k)
-    nclusters.bc[k]=index.G1(t(data.16s.denoized),data.cluster_temp,  d = data.dist.bc,
+    nclusters.bc[k]=index.G1(t(data.denoized),data.cluster_temp,  d = data.dist.bc,
                           centrotypes = "medoids")
   }
 }
-write.table(nclusters.bc,'CH_index_16s-pam-bray-3.tsv',sep = '\t',quote=F)
-pdf("CH_index_16s-pam-bray-3.pdf",height=4,width=5)
+write.table(nclusters.bc,'CH_index-pam-bray-2.tsv',sep = '\t',quote=F)
+pdf("CH_index-pam-bray-2.pdf",height=4,width=5)
 plot(nclusters.bc, type="b", xlab="k clusters", ylab="Calinski-Harabasz index",main="Optimal number of clusters",lwd = 2)
 dev.off()
-data.cluster.16s.bc=pam.clustering(data.dist.bc, k=2) 
-write.table(data.cluster.16s.bc,"cluster_16s_pam-3.tsv",sep = '\t',quote=F)
+data.cluster.bc=pam.clustering(data.dist.bc, k=2) 
+write.table(data.cluster.16s.bc,"cluster_pam-2.tsv",sep = '\t',quote=F)
 #silhouett
-obs.silhouette=mean(silhouette(data.cluster.16s.bc, data.dist.bc)[,3])
+obs.silhouette=mean(silhouette(data.cluster.bc, data.dist.bc)[,3])
 cat(obs.silhouette) #0.1351978
 ##pca
-obs.pca.16s.bc=dudi.pca(data.frame(t(data.16s.denoized)), scannf=F, nf = 2)
-s.class(obs.pca.16s.bc$li, fac=as.factor(data.cluster.16s.bc), grid=F,sub="Principal component analysis", col=c(3,2,4))
+obs.pca.bc=dudi.pca(data.frame(t(data.denoized)), scannf=F, nf = 2)
+s.class(obs.pca.bc$li, fac=as.factor(data.cluster.bc), grid=F,sub="Principal component analysis", col=c(3,2,4))
 ##bca
-obs.bet.16s.bc=bca(obs.pca.16s.bc, fac=as.factor(data.cluster.16s.bc), scannf=F, nf=2) 
+obs.bet.bc=bca(obs.pca.bc, fac=as.factor(data.cluster.bc), scannf=F, nf=2) 
 #s.class(obs.bet$ls, fac=as.factor(data.cluster), grid=F,sub="Between-class analysis", col=c(3,2))
 #pcoa
-obs.pcoa.16s.bc=dudi.pco(data.dist.bc,scannf=F, nf=2)
-s.class(obs.pcoa.16s.bc$li, fac=as.factor(data.cluster.16s.bc), grid=F,sub="Principal coordiante analysis", col=c(3,2,4))
+obs.pcoa.bc=dudi.pco(data.dist.bc,scannf=F, nf=2)
+s.class(obs.pcoa.bc$li, fac=as.factor(data.cluster.bc), grid=F,sub="Principal coordiante analysis", col=c(3,2,4))
